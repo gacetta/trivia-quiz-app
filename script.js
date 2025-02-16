c52d5e062306907a1ac0a8e62c4551e7ca8cd5ce
gacetta <michael.gacetta@gmail.com>
2025-02-16 11:42:12 -0800

    add: fetch category, difficulty, question and answers and display

--- script.js
+++ script.js
@@ -11,16 +11,39 @@ async function getNewQuestion() {
         const data = await response.json();
         const questionData = data[categoryNumber]
 
-        // console.log(questionData)
-        console.log(questionData.category)
-        console.log(questionData.difficulty)
-        console.log(questionData.question.text)
-        console.log(questionData.correctAnswer)
-        console.log(questionData.incorrectAnswers)
+        // assign category
+        const categoryElement = document.getElementById("category");
+        categoryElement.textContent = questionData.category
 
+        // assign difficulty
+        const difficultyElement = document.getElementById("difficulty");
+        difficultyElement.textContent = questionData.difficulty
+
+        // assign question
+        const questionElement = document.getElementById("question");
+        questionElement.textContent = questionData.question.text
+
+        // generate answer buttons
+        const answers = Array(questionData.correctAnswer, ...questionData.incorrectAnswers)
+        generateButtons(answers);
 
         const answerContainer = document.getElementById("answerContainer");
     } catch (err) {
         console.log("Unable to fetch from API: ", err)
     }
+}
+
+const generateButtons = (answers) => {
+    // locate container
+    const answerButtonContainer = document.getElementById("answerBtnContainer");
+    // erase previous buttons
+    answerButtonContainer.innerHTML = ""
+
+    // iterate over answers and generate buttons
+    answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.textContent = answer;
+        button.addEventListener('click', () => console.log(`you clicked ${answer}`))
+        answerButtonContainer.appendChild(button)
+    });
 }
